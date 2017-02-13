25464ce6427880db692912177f55c0ef9047107c
Paul Milham <paul.milham@wildworks.com>
2017-02-13 16:47:16 -0700

    🌸 add promise support and string updates

--- src/type/string.js
+++ src/type/string.js
@@ -3,21 +3,34 @@
 module.exports = {
   name: "string",
   validate(value, args, callback) {
-    // strings should be case insensitive
-    if (args) {
-      args = args.map(arg => arg.toLowerCase());
+    // parse arguments
+    if (!args || !args.length) {
+      throw new Error("string must be supplied a list of accepted constants or a max length");
     }
 
+    // string constants should be case insensitive
     if (typeof value !== "string") {
       callback(false, "not a string");
       return;
     }
-    // args is a list of acceptable strings
-    if (args && !args.includes(value.toLowerCase())) {
-      callback(false, "not in list of acceptable args");
-      return;
+
+    args = args.map(arg => arg.toLowerCase());
+
+    const maxLength = Number.parseInt(args[0]);
+    if (args.length > 1 || Number.isNaN(maxLength)) {
+      if (!args.includes(value.toLowerCase())) {
+        callback(false, `not in list of accepted constants (${args.join(", ")})`);
+        return;
+      }
+      callback(true, value.toLowerCase());
     }
-    callback(true);
+    else {
+      if (value.length > maxLength) {
+        callback(false, `longer than accepted max length of ${maxLength}`);
+        return;
+      }
+      callback(true);
+    }
   },
   parseDefault: null,
 };
